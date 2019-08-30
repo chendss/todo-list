0d05f5256ee7e95c7b129c03f150b5ad5a4d9dc9
dashao <capslock.ch@outlook.com>
2019-08-31 02:18:27 +0800

    loading svg

--- client/src/main.js
+++ client/src/main.js
@@ -1,29 +1,28 @@
 import Vue from 'vue'
-import routers from './Route'
-import App from './App.vue'
 import store from '@store'
+import App from './App.vue'
+import routers from './Route'
 import VueRouter from 'vue-router'
-import { get } from '@utils'
 
-const vueUse = function () {
-  Vue.use(VueRouter)
+const vueUse = function() {
+	Vue.use(VueRouter)
 }
 
-const vueSetting = function () {
-  Vue.config.productionTip = false
+const vueSetting = function() {
+	Vue.config.productionTip = false
 }
 
-const vueInit = function () {
-  new Vue({
-    store,
-    router: new VueRouter(routers),
-    render: h => h(App),
-  }).$mount('#app')
+const vueInit = function() {
+	new Vue({
+		store,
+		router: new VueRouter(routers),
+		render: h => h(App),
+	}).$mount('#app')
 }
 
-const main = function () {
-  vueUse()
-  vueSetting()
-  vueInit()
+const main = function() {
+	vueUse()
+	vueSetting()
+	vueInit()
 }
 main()
