e26c8b1ef9ebbfad1ccf1c5bce3fc194eedf5405
dashao <capslock.ch@outlook.com>
2019-10-05 23:36:16 +0800

    不清除

--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -1,18 +1,14 @@
-import { DB } from '@utils/index'
-
 export default {
-  data () {
-    return {}
-  },
-  created () {
-    DB.clear()
-  },
-  methods: {
-    register () {
-      this.$router.push('/userInfo/register')
-    },
-    login () {
-      this.$router.push('/userInfo/login')
-    },
-  },
+	data() {
+		return {}
+	},
+	created() {},
+	methods: {
+		register() {
+			this.$router.push('/userInfo/register')
+		},
+		login() {
+			this.$router.push('/userInfo/login')
+		},
+	},
 }
