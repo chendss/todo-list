7bc3c1853a628a077e068909bd22d9692c884b73
dashao <capslock.ch@outlook.com>
2020-04-28 00:59:07 +0800

    修改跳转

--- client/src/Pages/UserInfo/userInfo.js
+++ client/src/Pages/UserInfo/userInfo.js
@@ -7,60 +7,61 @@ import Loading from '@utils/loading'
 const message = Message
 
 export default {
-	components: { Form, FormItem, ElementInput: Input, Button, Icon },
-	data() {
-		const userInfo = DB.get('userInfo')
-		return {
-			param: {
-				username: get(userInfo, 'username', ''),
-				password: get(userInfo, 'password', ''),
-				confirmPassword: '',
-			},
-			rules: {
-				username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-				password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-				confirmPassword: [{ validator: this.confirmPwdValidator, trigger: 'change' }],
-			},
-		}
-	},
-	computed: {
-		type() {
-			return get(this, '$route.params.type', 'login')
-		},
-	},
-	methods: {
-		confirmPwdValidator(rule, value, callback) {
-			if (value === '') {
-				callback(new Error('请再次输入密码'))
-			} else if (value !== this.param.password) {
-				callback(new Error('两次输入密码不一致!'))
-			} else {
-				callback()
-			}
-		},
-		submitForm() {
-			const msg = get(this, 'type', 'login') === 'login' ? '登录成功' : '注册成功'
-			this.$refs.user.validate(async valid => {
-				if (valid) {
-					const { username, password } = this.param
-					if (username === 'admin' && password === 'admin') {
-						DB.set('token', { token: 'debug' })
-						this.$router.push('/list/')
-					} else {
-						Loading.open()
-						await action(this.type, username, password)
-						Loading.close()
-						message.success({ message: msg, duration: 1000 })
-						DB.set('userInfo', { password, username })
-						this.$router.push('/list/')
-					}
-				}
-			})
-		},
-		userChange() {
-			const type = get(this, '$route.params.type', 'login')
-			const suffix = type === 'login' ? 'register' : 'login'
-			this.$router.push(`/userInfo/${suffix}`)
-		},
-	},
+  components: { Form, FormItem, ElementInput: Input, Button, Icon },
+  data () {
+    const userInfo = DB.get('userInfo')
+    return {
+      param: {
+        username: get(userInfo, 'username', ''),
+        password: get(userInfo, 'password', ''),
+        confirmPassword: '',
+      },
+      rules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+        confirmPassword: [{ validator: this.confirmPwdValidator, trigger: 'change' }],
+      },
+    }
+  },
+  computed: {
+    type () {
+      return get(this, '$route.params.type', 'login')
+    },
+  },
+  methods: {
+    confirmPwdValidator (rule, value, callback) {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.param.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    },
+    submitForm () {
+      const msg = get(this, 'type', 'login') === 'login' ? '登录成功' : '注册成功'
+      this.$refs.user.validate(async valid => {
+        if (valid) {
+          const { username, password } = this.param
+          if (username === 'admin' && password === 'admin') {
+            DB.set('token', { token: 'debug' })
+          } else {
+            Loading.open()
+            await action(this.type, username, password)
+            Loading.close()
+            message.success({ message: msg, duration: 1000 })
+            DB.set('userInfo', { password, username })
+          }
+          setTimeout(() => {
+            this.$router.push('/list/')
+          }, 300)
+        }
+      })
+    },
+    userChange () {
+      const type = get(this, '$route.params.type', 'login')
+      const suffix = type === 'login' ? 'register' : 'login'
+      this.$router.push(`/userInfo/${suffix}`)
+    },
+  },
 }
